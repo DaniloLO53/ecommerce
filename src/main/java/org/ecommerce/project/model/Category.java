96843a11af27467279ef32e8b6ece6dd9cc7efc3
DaniloLO53 <daniloleaodev@gmail.com>
2025-05-28 08:57:32 -0300

    feat & install: lombok and Category lombok parameters added

--- src/main/java/org/ecommerce/project/model/Category.java
+++ src/main/java/org/ecommerce/project/model/Category.java
@@ -4,36 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
     private String name;
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Category() {
-        
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/org/ecommerce/project/repository/CategoryRepository.java
+++ src/main/java/org/ecommerce/project/repository/CategoryRepository.java
@@ -2,7 +2,6 @@ package org.ecommerce.project.repository;
 
 import org.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
