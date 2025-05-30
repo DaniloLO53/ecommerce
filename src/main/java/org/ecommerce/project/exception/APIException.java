3fc32f8accafe50b941c679b4560abe95f7e1406
DaniloLO53 <daniloleaodev@gmail.com>
2025-05-30 15:48:56 -0300

    refactor: clean up

--- src/main/java/org/ecommerce/project/exception/APIException.java
+++ src/main/java/org/ecommerce/project/exception/APIException.java
@@ -1,8 +1,6 @@
 package org.ecommerce.project.exception;
 
 public class APIException extends RuntimeException {
-//    private static final Long SERIAL_VERSION_UID = 1L;
-
     public APIException() {
     }
 
