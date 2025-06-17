6767afe5ddf327381fe759c3fb68041ea42d7562
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-17 15:33:10 -0300

    feat: UserDetailsService implementation

--- src/main/java/org/ecommerce/project/repositories/ProductRepository.java
+++ src/main/java/org/ecommerce/project/repositories/ProductRepository.java
@@ -5,7 +5,9 @@ import org.ecommerce.project.models.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategory(Category category, Pageable pageDetails);
     Page<Product> findByTitleLikeIgnoreCase(String keyword, Pageable pageDetails);
---
+++ src/main/java/org/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package org.ecommerce.project.security.services;
+
+import jakarta.transaction.Transactional;
+import org.ecommerce.project.models.User;
+import org.ecommerce.project.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional // enables rollback (db changes only if operation is successful)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        if (optionalUser.isPresent()) {
+            return UserDetailsImpl.build(optionalUser.get());
+        }
+
+        throw new UsernameNotFoundException("Username not found");
+    }
+}
