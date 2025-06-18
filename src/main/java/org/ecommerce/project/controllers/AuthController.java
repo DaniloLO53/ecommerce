bd024f48bb87cc02d7e54aa8fc6ca32b162d39cc
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-18 10:49:19 -0300

    feat: auth controller for signup

--- src/main/java/org/ecommerce/project/controllers/AuthController.java
+++ src/main/java/org/ecommerce/project/controllers/AuthController.java
@@ -1,7 +1,17 @@
 package org.ecommerce.project.controllers;
 
+import jakarta.validation.Valid;
+import org.ecommerce.project.exceptions.APIException;
+import org.ecommerce.project.exceptions.ResourceNotFoundException;
+import org.ecommerce.project.models.Role;
+import org.ecommerce.project.models.RoleName;
+import org.ecommerce.project.models.User;
+import org.ecommerce.project.payloads.responses.APIResponse;
 import org.ecommerce.project.payloads.responses.LoginRequest;
 import org.ecommerce.project.payloads.responses.LoginResponse;
+import org.ecommerce.project.payloads.responses.SignupRequest;
+import org.ecommerce.project.repositories.RoleRepository;
+import org.ecommerce.project.repositories.UserRepository;
 import org.ecommerce.project.security.jwt.JwtUtils;
 import org.ecommerce.project.security.services.UserDetailsImpl;
 import org.springframework.http.HttpStatus;
@@ -12,25 +22,34 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final JwtUtils jwtUtils;
+    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public AuthController(JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+    public AuthController(JwtUtils jwtUtils,
+                          PasswordEncoder passwordEncoder,
+                          AuthenticationManager authenticationManager,
+                          UserRepository userRepository,
+                          RoleRepository roleRepository) {
         this.jwtUtils = jwtUtils;
+        this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @PostMapping("/signin")
@@ -59,5 +78,62 @@ public class AuthController {
         LoginResponse response = new LoginResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtToken);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody @Valid SignupRequest signupRequest) {
+        String username = signupRequest.getUsername();
+        String email = signupRequest.getEmail();
+        String password = signupRequest.getPassword();
+
+        Set<String> strRoles = signupRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+
+        Boolean usernameAlreadyExists = userRepository.existsByUserName(signupRequest.getUsername());
+        Boolean emailAlreadyExists = userRepository.existsByEmail(signupRequest.getEmail());
+
+        if (usernameAlreadyExists || emailAlreadyExists) {
+            throw new APIException("Username or email already exists");
+        }
+
+        // TODO: optimize to make only one request to db and change switch-case to a streams solution (convert strings set to enums set)
+        if (strRoles == null || strRoles.isEmpty()) {
+            Role userRole = roleRepository
+                    .findByRoleName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new ResourceNotFoundException("Role", "name", RoleName.ROLE_USER.name()));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role dbRoleAdmin = roleRepository
+                                .findByRoleName(RoleName.ROLE_ADMIN)
+                                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", RoleName.ROLE_USER.name()));
+
+                        roles.add(dbRoleAdmin);
+                        break;
+                    case "seller":
+                        Role dbRoleSeller = roleRepository
+                                .findByRoleName(RoleName.ROLE_SELLER)
+                                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", RoleName.ROLE_SELLER.name()));
+
+                        roles.add(dbRoleSeller);
+                        break;
+                    default:
+                        Role dbRoleUser = roleRepository
+                                .findByRoleName(RoleName.ROLE_USER)
+                                .orElseThrow(() -> new ResourceNotFoundException("Role", "name", RoleName.ROLE_USER.name()));
+
+                        roles.add(dbRoleUser);
+                }
+            });
+        }
+
+        User user = new User(username, email, passwordEncoder.encode(password));
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new APIResponse("User has been created successfully"));
     }
 }
