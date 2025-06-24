2fb65b705844c5176998ece28121c68c38f2eb9f
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-24 13:37:44 -0300

    feat: add product to cart endpoint

--- src/main/java/org/ecommerce/project/payloads/DTOs/CartProductMetadataDTO.java
+++ src/main/java/org/ecommerce/project/payloads/DTOs/CartProductMetadataDTO.java
@@ -1,20 +1,25 @@
 package org.ecommerce.project.payloads.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartProductMetadataDTO {
+    public CartProductMetadataDTO(ProductDTO product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
     private Long id;
 
-    private ProductDTO productDTO;
+    private ProductDTO product;
 
     private Integer quantity;
-
-    private Set<CartDTO> cartDTOS;
 }
