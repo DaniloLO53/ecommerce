1c85b14b2a5f1a52a2b088acb38275d2f67fd9a4
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-24 08:54:33 -0300

    feat: dtos for all models (it will probably be changed)

---
+++ src/main/java/org/ecommerce/project/payloads/DTOs/UserDTO.java
@@ -0,0 +1,36 @@
+package org.ecommerce.project.payloads.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20, message = "Username must have less than 20 characters")
+    private String username;
+
+    @NotBlank
+    @Size(max = 50, message = "Email must have less than 50 characters")
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private Set<RoleDTO> roleDTOS;
+
+    private Set<ProductDTO> productDTOS;
+
+    private List<AddressDTO> addressDTOS;
+
+    private CartDTO cartDTO;
+}
