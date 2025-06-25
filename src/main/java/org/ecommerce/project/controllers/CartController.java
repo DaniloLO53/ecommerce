55690e5307e6bc1b47e08e163ea9828588c26b80
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-25 09:31:24 -0300

    feat: update cart product quantity

--- src/main/java/org/ecommerce/project/controllers/CartController.java
+++ src/main/java/org/ecommerce/project/controllers/CartController.java
@@ -38,6 +38,17 @@ public class CartController {
         return ResponseEntity.status(HttpStatus.OK).body(cartService.getUserCart(userId));
     }
 
+    @PutMapping("/cart/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> updateProductQuantity(
+            Authentication authentication,
+            @PathVariable Long productId,
+            @PathVariable Integer quantity) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Long userId = userDetails.getId();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartService.updateProductQuantity(userId, productId, quantity));
+    }
+
     @GetMapping("/carts")
     public ResponseEntity<List<CartDTO>> getAllCarts() {
         return ResponseEntity.status(HttpStatus.OK).body(cartService.getAllCarts());
