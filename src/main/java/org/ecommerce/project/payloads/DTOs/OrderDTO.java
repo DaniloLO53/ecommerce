f3b7553b5672e70ce003afa57b072812eafa8a5a
DaniloLO53 <daniloleaodev@gmail.com>
2025-06-26 08:35:59 -0300

    feat: created dtos for entities and OrderRequest

---
+++ src/main/java/org/ecommerce/project/payloads/DTOs/OrderDTO.java
@@ -0,0 +1,32 @@
+package org.ecommerce.project.payloads.DTOs;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class OrderDTO {
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    private LocalDate orderDate;
+
+    @NotNull
+    @Positive
+    private Double totalAmount;
+
+    @ToString.Exclude
+    private Set<OrderItemDTO> orderItems = new HashSet<>();
+
+    private PaymentDTO payment;
+
+    private AddressDTO address;
+}
